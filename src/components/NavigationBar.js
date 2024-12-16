93e0cdefd5744954c5dd1fa96b9d4263ad8a8e8b
김기진 <abjin69@gmail.com>
2024-12-16 20:23:57 +0900

    fix nav bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './NavigationBar.css';
+import { useNavigate } from 'react-router-dom';
 
 const NavigationBar = ({ CarmeraElement }) => {
+  const navigate = useNavigate();
+
+  const onClickDiet = () => navigate('/nutrition');
+
+  const onClickHabit = () => navigate('/habit');
+
+  const onClickMyPage = () => navigate('/mypage');
+
+  const onClickInquiry = () => {};
+
   return (
     <div className="navigation-bar">
       {/* 왼쪽 섹션: 식단, 습관 */}
       <div className="nav-left">
-        <div className="nav-item">
+        <div className="nav-item" onClick={onClickDiet}>
           <img src="/nutrition-icon.png" alt="식단" className="nav-icon" />
           <span className="nav-label">식단</span>
         </div>
-        <div className="nav-item">
+        <div className="nav-item" onClick={onClickHabit}>
           <img src="/habit-icon.png" alt="습관" className="nav-icon" />
           <span className="nav-label">습관</span>
         </div>
@@ -18,7 +29,7 @@ const NavigationBar = ({ CarmeraElement }) => {
       {CarmeraElement}
       {/* 오른쪽 섹션: 마이페이지 */}
       <div className="nav-right">
-        <div className="nav-item">
+        <div className="nav-item" onClick={onClickMyPage}>
           <img src="/mypage-icon.png" alt="마이페이지" className="nav-icon" />
           <span className="nav-label">마이페이지</span>
         </div>
