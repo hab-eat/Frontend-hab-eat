8e5e0779684b247652399f602b4318cf2bf5a02e
김기진 <abjin69@gmail.com>
2024-12-16 22:20:56 +0900

    delete comment

--- src/api/index.js
+++ src/api/index.js
@@ -79,21 +79,15 @@ class Api extends BaseRestApi {
 
   async uploadImageToSignedUrl({ signedUrl, file, type }) {
     return axios
-      .put(signedUrl, file, {
-        headers: {
-          'Content-Type': type,
-        },
-      })
+      .put(signedUrl, file, { headers: { 'Content-Type': type } })
       .then((response) => response.data);
   }
 
   async getKakaoAccessToken(code) {
     console.log('getKakaoAccessToken');
-    const redirectUrl = process.env.REACT_APP_KAKAO_REDIRECT_URL; // 카카오 개발자 콘솔에 등록된 Redirect URI
-    const Rest_api_key = process.env.REACT_APP_KAKAO_REST_API_KEY; // 카카오 REST API 키
-
+    const redirectUrl = process.env.REACT_APP_KAKAO_REDIRECT_URL;
+    const Rest_api_key = process.env.REACT_APP_KAKAO_REST_API_KEY;
     return axios.post('https://kauth.kakao.com/oauth/token', null, {
-      // headers: { 'Content-Type': 'application/json' },
       params: {
         grant_type: 'authorization_code',
         client_id: Rest_api_key,
