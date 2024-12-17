2e5c4cccc01843c1c902c259b9c4f27f500a5b33
김기진 <abjin69@gmail.com>
2024-12-17 10:36:03 +0900

    feat: 음식 자동 완성 & 입력 페이지 라우터 추가

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ import ChallengeCamera from './pages/ChallengeCamera';
 import FoodAnalysisResultPage from './pages/FoodAnalysisResultPage';
 import FoodAnalysisCheckPage from './pages/FoodAnalysisCheckPage';
 import FoodAnalysisRetryPage from './pages/FoodAnalysisRetryPage';
+import FoodSelectionPage from './pages/FoodSelectionPage';
+import QuantityAdjustPage from './pages/QuantityAdjustPage';
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false); // 로그인 여부 상태
@@ -78,6 +80,8 @@ const App = () => {
             path="/analysis/food-image/retry"
             element={<FoodAnalysisRetryPage />}
           />
+          <Route path="/food/autocomplete" element={<FoodSelectionPage />} />
+          <Route path="/food/input-form" element={<QuantityAdjustPage />} />
           <Route path="/challenge/camera" element={<ChallengeCamera />} />
         </Routes>
       </div>
