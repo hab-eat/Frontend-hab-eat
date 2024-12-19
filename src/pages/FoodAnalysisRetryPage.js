16bc2566055e54c9b42f43429d452f15ee55d48e
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-12-19 15:21:48 +0900

    주석 제거

--- src/pages/FoodAnalysisRetryPage.js
+++ src/pages/FoodAnalysisRetryPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './FoodAnalysisCheckPage.css'; // ResultSecond.js 재사용
+import './FoodAnalysisCheckPage.css';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const FoodAnalysisRetryPage = () => {
